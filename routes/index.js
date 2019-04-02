c208b0c6872760234c7c78bf9fbbca43b7890513
Yeeun <yesung000@naver.com>
2019-04-02 10:59:52 +0900

    [api] mysql get/post test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+const app = express();
+
+var con = mysql.createConnection({
+  host: "shim-db.ck6dpjl28utz.ap-northeast-2.rds.amazonaws.com",
+  user: "shinshim",
+  password: "Shim0323!",
+  database: "SHIM"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:name', function(req, res) {
+  param = [req.params.name]
+  let selectQuery = 'SELECT main_id FROM SHIM.MAIN_TB WHERE MAIN_TB.main_name = ?;'
+  con.query(selectQuery, param, function(err, result){
+    console.log(result)
+    if (err){
+      res.status(500).send({
+        message: "Error"
+      })
+    }
+    else {
+      res.status(201).send(result)
+    }
+  })
 });
+
+router.post("/", function (req, res){
+  id = req.body.id
+
+  let selectQuery = 'SELECT main_name from SHIM.MAIN_TB WHERE main_id = ?'
+  con.query(selectQuery, id, function(err, result){
+    console.log(result)
+    if (err){
+      res.status(500).send({
+        message: "Error"
+      })
+    }
+    else {
+      res.status(201).send(result)
+    }
+  })
+})
+
 
 module.exports = router;
