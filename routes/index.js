31f561660164692df1884cab105a5acfdbcbc334
Yeeun <yesung000@naver.com>
2019-04-06 15:54:18 +0900

    [api] routes change

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-const app = express();
-require('dotenv').config();
 
-var con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  // console.log("Connected!")
-})
-
-/* GET home page. */
-router.get('/:name', function(req, res) {
-  param = req.params.name
-  let selectQuery = 'SELECT main_id FROM SHIM.MAIN_TB WHERE MAIN_TB.main_name = ?;'
-  con.query(selectQuery, param, function(err, result){
-    if (err){
-      res.status(500).send({
-        message: "Error"
-      })
-    }
-    else {
-      res.status(201).send(result)
-    }
-  })
-});
-
-router.post("/", function (req, res){
-  id = req.body.id
-
-  let selectQuery = 'SELECT main_name from SHIM.MAIN_TB WHERE main_id = ?'
-  con.query(selectQuery, id, function(err, result){
-    console.log(result)
-    if (err){
-      res.status(500).send({
-        message: "Error"
-      })
-    }
-    else {
-      res.status(201).send(result)
-    }
-  })
-})
-
+router.use('/main', require('./main.js'))
+router.use('/sleep', require('./sleep.js'))
+router.use('/video', require('./video.js'))
+router.use('/music', require('./music.js'))
+router.use('/etc', require('./etc.js'))
 
 module.exports = router;
---
+++ routes/music.js
@@ -0,0 +1,50 @@
+// music.js
+module.exports = (app) => {
+  const express = require('express');
+  const router = express.Router();
+  const mysql = require('mysql2/promise');
+
+  const pool = app.get('pool')
+
+  const selectMainId = async (name) => {
+    try {
+      const connection = await pool.getConnection()
+      const [result] = await connection.query('SELECT main_id FROM SHIM.MAIN_TB WHERE MAIN_TB.main_name = ?;', [name])
+      return result
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  const selectMainName = async (id) => {
+    try {
+      const connection = await pool.getConnection()
+      const [result] = await connection.query('SELECT main_name FROM SHIM.MAIN_TP WHERE MAIN_TB.main_id = ?;', [id])
+      return result
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  router.get('/:name', async (req, res) => {
+    const name = req.params.name
+    try {
+      const result = await selectMainId(name)
+      res.status(200).json({ 'status': 200, 'arr': result }) // arr, data, msg
+    } catch (err) {
+      res.status(500).json({ 'status': 500, 'msg': 'error!' })
+    }
+  })
+
+  router.post("/", async (req, res) => {
+    const id = req.body.id
+    try {
+      const result = await selectMainName(id)
+      res.status(201).json({ 'status': 201, 'arr': result })
+    } catch (err) {
+      res.status(500).json({ 'status': 500, 'msg': 'error!' })
+    }
+  })
+
+  return router
+}
