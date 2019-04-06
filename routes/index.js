73a7845692b5645a6c43b1a72f806557faad8897
Yeeun <yesung000@naver.com>
2019-04-06 15:57:48 +0900

    [api] index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/main', require('./main.js'))
-router.use('/sleep', require('./sleep.js'))
-router.use('/video', require('./video.js'))
-router.use('/music', require('./music.js'))
-router.use('/etc', require('./etc.js'))
+//router.use('/main', require('./main.js'))
+//router.use('/sleep', require('./sleep.js'))
+//router.use('/video', require('./video.js'))
+//router.use('/music', require('./music.js'))
+//router.use('/etc', require('./etc.js'))
 
 module.exports = router;
