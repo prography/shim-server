08ef7c10a3f466a49213159e070368894aeb3b7f
Yeeun <yesung000@naver.com>
2019-06-28 16:53:56 +0900

    [api] my api(1)

--- routes/music.js
+++ routes/music.js
@@ -21,11 +21,53 @@ module.exports = (app) => {
     }
   }
 
-  router.get('/:category', async (req, res) => {
-    const category = req.params.category // all, sleep, instrument, nature
+  const selectMyInfo = async (user_id) => {
     try {
-      const result = await selectByCategory(category)
+      const connection = await pool.getConnection()
+      const result = await connection.query('SELECT music_id, music_name, music_music, music_author, music_picture FROM SHIM.MUSIC_TB, SHIM.MY_TB WHERE MY_TB.my_user_id like ? AND MY_TB.my_music_id = MUSIC_TB.music_id;', [user_id])
+      connection.release()
+      return result[0]
+    } catch (err) {
+      connection.release()
+      throw new Error(err)
+    }
+  }
+
+  const insertMyMusic = async (user_id, music_id) => {
+    try {
+      const connection = await pool.getConnection()
+      const result = await connection.query('INSERT INTO SHIM.MY_TB (my_user_id, my_music_id) VALUES (?, ?);', [user_id, music_id])
+      connection.release()
+      return true
+    } catch (err) {
+      connection.release()
+      throw new Error(err)
+    }
+  }
+
+  router.get('/:category', async (req, res) => {
+    const category = req.params.category // all, my, relax, focus
+    try {
+      let result
+      if (category === 'my') {
+        const user_id = req.query.id
+        result = await selectMyInfo(user_id)
+      } else {
+        console.log(category)
+        result = await selectByCategory(category)
+      }
       res.status(200).json({ 'status': 200, 'arr': result }) // arr, data, msg
+    } catch (err) {
+      res.status(500).json({ 'status': 500, 'msg': 'error!' })
+    }
+  })
+
+  router.post('/', async (req, res) => {
+    try {
+      const user_id = req.body.user_id
+      const music_id = req.body.music_id
+      await insertMyMusic(user_id, music_id)
+      res.status(200).json({ 'status': 200, 'msg': 'ok!' })
     } catch (err) {
       res.status(500).json({ 'status': 500, 'msg': 'error!' })
     }
