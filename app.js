28f14a45d3ebb8e16eb4e558240fd241f91781c7
PSJ <astro.psj@gmail.com>
2019-09-24 21:06:40 +0900

    Update package

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set('view engine', 'pug')
 
 require('dotenv').config()
 const pool = mysql.createPool({
