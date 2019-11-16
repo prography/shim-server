13e4c42df81fecfdf08238df2ac475ec0ed0c575
Yeeun <yesung000@naver.com>
2019-11-16 15:52:41 +0900

    database from .env

--- src/database.js
+++ src/database.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
 
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'test',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 20,
   queueLimit: 0,
 }).promise();
 
